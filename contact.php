a259744df3a941e23006d3e0237ee725d2eb1bf9
dcastane <davy.castanet@gmail.com>
2016-09-06 08:56:30 -0400

    push initial

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+	// Initialisation ici
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+<div id="wrapper">
+<?php 
+	echo '';
+?>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dcastane
+ * Date: 2016-09-06
+ * Time: 08:17
+ */
